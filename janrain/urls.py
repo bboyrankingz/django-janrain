0c41bb91005e3af05eaa2057ceff2836ef1dfd39
bboy.stats <bboy.stats@gmail.com>
2015-01-25 14:58:15 +0100

    add loginpage in urls

--- janrain/urls.py
+++ janrain/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('janrain.views',
     url(r'^login/$', 'login', name='janrain_login'),
     url(r'^logout/$', 'logout', name='janrain_logout'),
+    url(r'^loginpage/$', 'loginpage', name='janrain_loginpage'),
 )
